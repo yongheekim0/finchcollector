87260e4ec85cf66c234b055a0fd5afddf9b61e52
Yonghee Kim <19yhk91@gmail.com>
2023-12-11 20:07:48 +0900

    Add feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner'),
+)
 # Create your models here.
 class Finch(models.Model):
   name = models.CharField(max_length=100)
@@ -7,5 +13,26 @@ class Finch(models.Model):
   description = models.TextField(max_length=300)
   diet = models.CharField(max_length=100)
 
+
+  def get_absolute_url(self):
+    return reverse('detail', kwargs={'finch_id': self.id})
+  
   def __str__(self):
     return self.name
+  
+
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(
+    max_length=1,
+    # add the 'choices' field option
+    choices=MEALS,
+    # set the default value for meal to be 'B'
+    default=MEALS[0][0]
+  )
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.get_meal_display()} on {self.date}'
+  class Meta:
+    ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
@@ -15,4 +17,29 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', {'finch': finch})
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form': feeding_form})
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['habitat', 'description', 'diet']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
+
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the finch_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
